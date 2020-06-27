0f65d8acedef5347b2e39d661c8d801e293a1e19
yfzhe <yfangzhe@gmail.com>
2020-06-27 15:44:54 +0800

    pkg: fix deps

--- info.rkt
+++ info.rkt
@@ -3,7 +3,8 @@
 (define collection "eval-bot")
 
 (define deps '("base"
-               "sandbox-lib"))
+               "sandbox-lib"
+               "web-server-lib"))
 (define build-deps '())
 
 (define version "0.3")
