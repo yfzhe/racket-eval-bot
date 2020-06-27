31061742045f481d7392a034437213c93ad9f01b
yfzhe <yfangzhe@gmail.com>
2020-06-27 11:49:11 +0800

    util: add `await` sugar

--- util.rkt
+++ util.rkt
@@ -1,7 +1,8 @@
 #lang racket/base
 (require (for-syntax racket/base))
 
-(provide hash-ref*)
+(provide hash-ref*
+         await await/proc)
 
 (define-syntax (hash-ref* stx)
   (syntax-case stx ()
@@ -9,3 +10,18 @@
      #'(hash-ref hash key)]
     [(_ hash key more ...)
      #'(hash-ref* (hash-ref hash key) more ...)]))
+
+;; inspired by `async-task` and `async` from misc1/async
+(define (await/proc thunk)
+  ;; TODO: make a custom event otherwise a channel
+  (define ch (make-channel))
+  (thread
+   (lambda ()
+     (let ([res (thunk)])
+       (channel-put ch res))))
+  ch)
+
+(define-syntax (await stx)
+  (syntax-case stx ()
+    [(_ body ...)
+     #'(await/proc (lambda () body ...))]))
