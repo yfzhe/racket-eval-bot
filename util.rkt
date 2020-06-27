c5249eab39e32f5dfcdcfbb0050f6a475fa3aa4c
yfzhe <yfangzhe@gmail.com>
2020-06-27 12:32:08 +0800

    util: add optional chain `hash-ref**`

--- util.rkt
+++ util.rkt
@@ -4,6 +4,8 @@
 (provide hash-ref*
          await await/proc)
 
+;; hash-ref*: chainning ref nested hash keys
+;; NOTE: it seems that these two does not need to be macros
 (define-syntax (hash-ref* stx)
   (syntax-case stx ()
     [(_ hash key)
@@ -11,6 +13,16 @@
     [(_ hash key more ...)
      #'(hash-ref* (hash-ref hash key) more ...)]))
 
+;; hash-ref**: some kind of "optional chain"
+(define-syntax (hash-ref** stx)
+  (syntax-case stx ()
+    [(_ hash key)
+     #'(hash-ref hash key #f)]
+    [(_ hash key more ...)
+     #'(let ([val (hash-ref hash key #f)])
+         (and val (hash-ref** val more ...)))]))
+
+;; await: execute in another thread
 ;; inspired by `async-task` and `async` from misc1/async
 (define (await/proc thunk)
   ;; TODO: make a custom event otherwise a channel
