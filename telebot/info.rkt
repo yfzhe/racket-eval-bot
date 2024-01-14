c1b0c63d9f0abf4cdd95d83318cc9b1c965e1ac0
yfzhe <yfangzhe@gmail.com>
2024-01-14 15:38:19 +0800

    telebot: bump version to 0.1

--- telebot/info.rkt
+++ telebot/info.rkt
@@ -8,7 +8,7 @@
 ;; (define scribblings '(("scribblings/telebot.scrbl" ())))
 
 (define pkg-desc "Telegram bot API")
-(define version "0.0")
+(define version "0.1")
 
 (define pkg-authors '(yfzhe))
 (define license '(Apache-2.0 OR MIT))
