21a8d2d783fc1d5157c771756119d8e0ff9efb99
yfzhe <yfangzhe@gmail.com>
2024-01-15 02:26:25 +0800

    telebot: add `bot-start/webhook`

--- telebot/main.rkt
+++ telebot/main.rkt
@@ -1,5 +1,9 @@
 #lang racket/base
-(require "private/bot.rkt"
+(require (only-in web-server/servlet
+                  response/empty request-post-data/raw)
+         web-server/servlet-env
+         json
+         "private/bot.rkt"
          "private/error.rkt"
          "schema.rkt"
          "api.rkt")
@@ -8,8 +12,7 @@
          exn:fail:bot? exn:fail:bot:api?
          (all-from-out "api.rkt")
          ref :
-         bot-set-webhook
-         bot-start/poll)
+         bot-start/poll bot-start/webhook)
 
 (define (bot-get-updates bot [offset 0])
   (bot-post bot "/getUpdates"
@@ -30,3 +33,23 @@
        (define resp (handle-update update))
        (bot-send-message bot resp)
        (loop (add1 (update-id update)) (cdr updates))])))
+
+(define (bot-start/webhook bot handle-update webhook-base port)
+  (define webhook-url
+    (string-append webhook-base "/webhook"))
+
+  (bot-set-webhook bot webhook-url)
+
+  (define (handle-webhook req)
+    (thread
+     (lambda ()
+       (handle-update
+        (jsexpr->update (bytes->jsexpr (request-post-data/raw req))))))
+
+    (response/empty #:code 200))
+
+  (serve/servlet handle-webhook
+                 #:port port
+                 #:listen-ip #f
+                 #:servlet-path "/webhook"
+                 #:command-line? #t))
