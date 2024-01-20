4671058b29aa5e430c99c980befacd97fc2d0080
yfzhe <yfangzhe@gmail.com>
2024-01-21 00:01:54 +0800

    meta: add license

--- telebot/info.rkt
+++ telebot/info.rkt
@@ -1,6 +1,8 @@
 #lang info
 
 (define collection "telebot")
+(define pkg-desc "Telegram bot API")
+(define version "0.2")
 
 (define deps '("base"
                "http-easy-lib"
@@ -9,8 +11,5 @@
 
 ;; (define scribblings '(("scribblings/telebot.scrbl" ())))
 
-(define pkg-desc "Telegram bot API")
-(define version "0.2")
-
 (define pkg-authors '(yfzhe))
-(define license '(Apache-2.0 OR MIT))
+(define license 'MIT)
