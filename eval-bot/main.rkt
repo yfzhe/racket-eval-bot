2aeed3d4bac0a2a58f06d8680a4fef12c8e75509
yfzhe <yfangzhe@gmail.com>
2024-02-25 00:04:07 +0800

    eval-bot: use `telebot` commands

--- eval-bot/main.rkt
+++ eval-bot/main.rkt
@@ -1,7 +1,6 @@
 #lang racket/base
 (require telebot
          telebot/text
-         telebot/command
          racket/match
          racket/string
          "eval.rkt")
@@ -14,6 +13,11 @@
   (error "does not setup BOT_TOKEN env-var"))
 (define bot (make-bot token))
 
+(bot-add-command! bot "start" start)
+(bot-add-command! bot "help" help)
+(bot-add-command! bot "eval" (eval 'racket))
+(bot-add-command! bot "eval_chez" (eval 'chez))
+
 (define (handle-update update)
   (cond
     [(ref (update : update) .message #f)
@@ -24,20 +28,11 @@
   (define in-private-chat?
     (equal? (ref (message : message) .chat .type) "private"))
   (cond
-    [(not text) (void)]
-    [(parse-command text (bot-username bot))
-     =>
-     (match-lambda
-       [(list "start" _) (start message)]
-       [(list "help" _) (help message)]
-       [(list "eval" code) (eval message code 'racket)]
-       [(list "eval_chez" code) (eval message code 'chez)]
-       [(list _ _) (bad-request message)])]
-    [in-private-chat?
-     (eval message text 'racket)]
+    [(and in-private-chat? text)
+     ((eval 'racket) bot message text)]
     [else (void)]))
 
-(define (start message)
+(define (start bot message _arg)
   (bot-send-message bot
                     #:chat-id (ref (message : message) .chat .id)
                     #:parse-mode "MarkdownV2"
@@ -47,7 +42,7 @@ Type your code, and wait for the result\.
 END
                     ))
 
-(define (help message)
+(define (help bot message _arg)
   (bot-send-message bot
                     #:chat-id (ref (message : message) .chat .id)
                     #:parse-mode "MarkdownV2"
@@ -59,18 +54,13 @@ END
 END
                     ))
 
-(define (bad-request message)
-  (bot-send-message bot
-                    #:chat-id (ref (message : message) .chat .id)
-                    #:parse-mode "MarkdownV2"
-                    #:text "Not supported command"))
-
 ;; mode: 'racket or 'chez
-(define (eval message code mode)
+(define ((eval mode) bot message code)
   (define eval-result
     (match mode
       ['racket (eval-code code)]
       ['chez (eval-code/chez code)]))
+
   (bot-send-message bot
                     #:chat-id (ref (message : message) .chat .id)
                     #:reply (make-reply-params
